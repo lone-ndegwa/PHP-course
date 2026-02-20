d1073b39966560ab966004239c9b8d2ba88fc737
lone_ndegwa <ndegwaeddie67@gmail.com>
2026-02-20 15:18:53 +0300

    Multi-array

--- arrays.php
+++ arrays.php
@@ -1,4 +1,12 @@
 <?php
 
 $fruits =["mango", "pawpaw", "pineapple"];
-echo $fruits[2];
+echo $fruits[2] . "<br>";
+
+$user = [
+    'name' => 'Zura',
+    'age' => '27',
+    'hobbies' => ['Swimming','Hiking','Reading'],
+];
+
+echo $user['name'];
